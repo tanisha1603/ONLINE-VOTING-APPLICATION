a4affe47eec479db5631515d3aeaa2d65c67b919
tanisha1603 <tanishatanishatiwari@gmail.com>
2026-04-07 20:37:07 +0530

    Update DBConnection.java

--- online voting application/DBConnection.java
+++ online voting application/DBConnection.java
@@ -1,1 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DBConnection {
+
+    public static Connection getConnection() {
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/voting_system",
+                    "root",
+                    "qwerty90");
+
+            return con;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
