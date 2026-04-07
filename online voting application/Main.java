57c61d3b1aa3ec6a5a48969f03d3945ed2e5a4bf
tanisha1603 <tanishatanishatiwari@gmail.com>
2026-04-07 20:37:57 +0530

    Update Main.java

--- online voting application/Main.java
+++ online voting application/Main.java
@@ -1,1 +1,11 @@
+import javax.swing.*;
 
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+        new LoginPage();
+    }
+}
